059e96cafcc845a6b525cf955ade12a3de4daac5
berat03 <bbulbulwork@gmail.com>
2023-08-19 17:32:31 +0100

    useful shape functions

---
+++ main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plt_circle(r=1):
+    theta = np.linspace(0, 2 * np.pi, 100)
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    plt.plot(x, y)
+    plt.axhline(0, color='black', linewidth=0.8)
+    plt.axvline(0, color='black', linewidth=0.8)
+    plt.axis('equal')
+    plt.show()
+
+
+def quadratic(power=2, range=2):
+    x = np.linspace(-range, range, 100)
+    y = x ** power
+    plt.plot(x,y)
+    plt.axhline(0, color='black', linewidth=0.8)
+    plt.axvline(0, color='black', linewidth=0.8)
+    plt.axis('equal')
+    plt.show()
+
